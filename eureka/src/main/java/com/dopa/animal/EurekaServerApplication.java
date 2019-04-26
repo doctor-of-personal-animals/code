0b46ee977c3a59cffa0241735815c9672cf65db9
yanlin <362460453@qq.com>
2019-04-26 23:17:41 +0800

    eureka server

---
+++ eureka/src/main/java/com/dopa/animal/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.dopa.animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yanlin
+ * @version v1.3
+ * @date 2019-04-26 10:41 PM
+ * @since v8.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
